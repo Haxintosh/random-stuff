d716321508839301c9eb2c837fdcbff19d03af37
Haxintosh <92402488+Haxintosh@users.noreply.github.com>
2023-10-13 01:34:17 +0000

    gud stuff

--- web/TO SUBMIT 13-10-23/jstest.js
+++ web/TO SUBMIT 13-10-23/jstest.js
@@ -3,18 +3,23 @@ function themechange() {
     let themeButton = document.getElementById("theme-button-button")
     const root = document.documentElement;
 
-    let dark = "#2f3741"
-    let white = "white";
+    let dark = "#282A36";
+    let white = "#F8F8F2";
+    let text_line_white = "gray";
+    let text_line_dark = "#cbcbcb"
+
 ;    // Check the current text content of the theme-icon
     if (themeIcon.textContent === "dark_mode") {
         // Change to "light_mode" when it's "dark_mode"
         themeIcon.textContent = "light_mode";
         root.style.setProperty('--bg-color', dark);
         root.style.setProperty('--icon-color', white);
+        root.style.setProperty('--input-line-color', text_line_dark);
     } else {
         // Change back to "dark_mode" when it's "light_mode"
         themeIcon.textContent = "dark_mode";
         root.style.setProperty('--bg-color', white);
-        root.style.setProperty('--icon-color', dark)
+        root.style.setProperty('--icon-color', dark);
+        root.style.setProperty('--input-line-color', text_line_white);
     }
 }
